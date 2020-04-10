62d2653e79a52f72e489d344c795eb12ed30df9f
Bill Kable <bkable@gmail.com>
2020-04-10 15:08:31 -0500

    fix postman spring cloud

--- scripts/install_spring_cloud.ps1
+++ scripts/install_spring_cloud.ps1
@@ -14,10 +14,7 @@ choco install git --confirm
 choco install openjdk11 --confirm
 choco install gradle --confirm
 choco install maven --confirm
-# choco install docker-desktop # Optional
-# choco install cloudfoundry-cli --confirm # Optional
 choco install jmeter --confirm
-choco install postman --confirm
 choco install curl --confirm
 choco install flyway.commandline --confirm
 choco install rabbitmq --confirm
@@ -25,6 +22,16 @@ choco install ntop.portable --confirm
 choco install intellijidea-community --confirm
 choco install vscode --confirm
 choco install springtoolsuite --confirm
+
+# Postman has special handling - imcompatible with Sysprep given it
+# installs to user profile, move it to root partition
+choco install postman --confirm
+cd ~/AppData/Local/Postman
+.\Postman.exe --squirrel-uninstall 7.20.4
+Stop-Process postman.exe
+cd 'C:\Program Files\Postman'
+.\Postman.exe --squirrel-install 7.20.4
+# You may need to manually set up startup shortcuts on cloned workspace!
 
 # Add trace plugin for Rabbitmq
 cd 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.3\sbin'
