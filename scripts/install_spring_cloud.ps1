2497c90d7d8fd78b206ef146896124b460223131
Bill Kable <bkable@gmail.com>
2020-04-13 18:02:59 -0500

    fix spring cloud install

--- scripts/install_spring_cloud.ps1
+++ scripts/install_spring_cloud.ps1
@@ -11,13 +11,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # Install prereq software
 
 choco install git --confirm
+choco install jmeter --confirm
 choco install openjdk11 --confirm
 choco install gradle --confirm
 choco install maven --confirm
-choco install jmeter --confirm
 choco install curl --confirm
-choco install flyway.commandline --confirm
-choco install rabbitmq --confirm
 choco install ntop.portable --confirm
 choco install intellijidea-community --confirm
 choco install vscode --confirm
@@ -27,12 +25,17 @@ choco install springtoolsuite --confirm
 # installs to user profile, move it to root partition
 choco install postman --confirm
 cd ~/AppData/Local/Postman
-.\Postman.exe --squirrel-uninstall 7.20.4
-Stop-Process postman.exe
-cd 'C:\Program Files\Postman'
-.\Postman.exe --squirrel-install 7.20.4
+.\Postman.exe --squirrel-uninstall 7.20.1
+Stop-Process -Name Postman
+cd 'C:\Program Files'
+mv ~/AppData/Local/Postman .
+cd Postman
+.\Postman.exe --squirrel-install 7.20.1
+Stop-Process -Name Postman
 # You may need to manually set up startup shortcuts on cloned workspace!
 
+# Rabbitmq
+choco install rabbitmq --confirm
 # Add trace plugin for Rabbitmq
 cd 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.3\sbin'
 .\rabbitmq-plugins enable rabbitmq_tracing
