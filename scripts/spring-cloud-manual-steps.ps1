449dbfbf8c983c5d9b8317283d5a6c8cc20a6b51
Bill Kable <bkable@gmail.com>
2020-04-15 21:46:49 -0500

    add manual step script

---
+++ scripts/spring-cloud-manual-steps.ps1
@@ -0,0 +1,19 @@
+# Install Steps
+
+choco install postman --confirm
+choco install rabbitmq --confirm
+cd 'C:\Program Files\RabbitMQ Server\rabbitmq_server-3.8.3\sbin'
+.\rabbitmq-plugins enable rabbitmq_tracing
+
+# -   Firefox Bookmarks
+# -   Add follow Desktop and Taskbar icons
+#    - Firefox
+#    - Powershell
+#    - Powershell ISE
+#    - Gitbash
+#    - Code
+#    - IntelliJ
+#    - STS
+#    - Postman
+
+
